7d33dac4251e4aba62fe011b9466bd27c1cf123c
SergioP <programingWeb2024@gmail.com>
2024-09-18 01:44:03 -0300

    Version 1

---
+++ includes/templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADOPT</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jura:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="site-header">
+        <div class="navegacion-principal">
+            <div class="logo">
+                <img src="img/logo.svg" alt="logotipo">
+            </div>
+            <nav class="barra">
+                <a href="#">Inicio</a>
+                <a href="#">Producto</a>
+                <a href="#">Nosotros</a>
+                <a href="#">Contacto</a>
+                <a href="#">Preventa</a>
+            </nav>
+        </div>
+        <div class="contenido-header">
+            <h1>Proyecto ADOPT</h1>
+            <p>El software ideal de administración para tu negocio.</p>
+        </div>
+    </header>
